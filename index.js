41b93fd8130c9086fa8308000bcba090884ea528
selam lemma <oneiconforlife26@gmail.com>
2020-10-11 17:19:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function takeANumber(line,name){
   }
   return `Welcome, ${name}. You are number ${line.indexOf(name)+1} in line.`
 }
+
+function nowServing(line){
+  if(line[0]){
+    let firstInLine = line[0];
+    line.shift();
+    return `Currently serving ${firstInLine}.`;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+
+}
